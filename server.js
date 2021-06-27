8a311312fc95f89f30d273bcbf4b49be41370b09
Batoolayyad <batoolayyad1996@gmail.com>
2021-06-27 16:21:43 +0300

    make tests

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+const express = require('express');
+const app = express();
+const notFoundHandler = require('./error-handlers/404');
+const errorHandler = require('./error-handlers/500');
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+app.get('/bad', (req, res) => {
+  throw new Error('Something went wrong');
+});
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+function start(port) {
+  app.listen(port, () => console.log(`Server is up on ${port}`));
+}
+module.exports = {
+  app: app,
+  start: start,
+};
